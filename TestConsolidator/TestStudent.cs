67c879937b281bbab2a24ec70d3b9fb5a6b6a70d
Leandro <Leandro.SARAIVA-MAIA@cpnv.ch>
2018-10-12 09:28:03 +0200

    Regroupement des élèves par intérets et par sex
    -Reçoit tous les élèves du CSV et les mets dans un Stack.
    -Tous les élèves sont comparé entre eux pour trouver leur affinité entre eux
    -Les affinités des élèves sont ensuite triée dans l'ordre croissant
    -Etablissement de listes d'élèves (nombre de groupes choisisable)

--- TestConsolidator/TestStudent.cs
+++ TestConsolidator/TestStudent.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Consolidator;
 
 namespace TestConsolidator
 {
@@ -7,9 +9,31 @@ namespace TestConsolidator
     public class TestStudent
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Student_Create_Sucess()
         {
-            //Assert.AreEqual();
+            //given
+            string name = "Adolf";
+            string sex = "0";
+            int interests = 3;
+            Stack<Student> students = new Stack<Student>(); ;
+            Student student;
+
+            //then
+            student = new Student(students, name, sex, interests);
+
+            //when
+            Assert.AreEqual(student.Name, name);
+            Assert.AreEqual(student.Interests, interests);
+
+            if (sex == "0")
+            {
+                sex = "Homme";
+            }
+            else if (sex == "1")
+            {
+                sex = "Femme";
+            }
+            Assert.AreEqual(student.SexString, sex);
         }
     }
 }
